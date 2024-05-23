9529e5fa8689c1c871674df41082056eb63a2dfc
TL\owaitmuh <muhammad.owais@rowriter.com>
2024-05-23 15:07:09 +0500

    .

--- src/app/services/env.ts
+++ src/app/services/env.ts
@@ -1,6 +1,6 @@
 
 export const environment = {
   production: false,
-  BASE_URL:'http://localhost:5000/api/', // 'https://backend.carcentric.com.au/api/', //
+  BASE_URL: 'https://backend.carcentric.com.au/api/', //'http://localhost:5000/api/', //
   BASE_URL2: 'http://cardrivenapp-env.eba-bt7vqbpa.ap-southeast-2.elasticbeanstalk.com/api/'
 }
